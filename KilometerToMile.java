aa7c144c5fdd1b77a13db0cee639f63a4bf36591
Anushka Gupta <anushkagupta@Mac.lan>
2025-01-19 22:18:13 +0530

    commit

---
+++ KilometerToMile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class KilometerToMile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+        double miles = kilometers * 0.621371;
+        System.out.println("The distance in miles is: " + miles);
+        sc.close();
+    }
+}
